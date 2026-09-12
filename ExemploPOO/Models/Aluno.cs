4947f2e862fe92eebb8d95908acf232fa7e290b4
RyanCarllos01 <ryancarllos.jac01@gmail.com>
2026-09-11 21:14:35 -0300

    feat(poo): adiciona exemplo de Programacao Orientada a Objetos

---
+++ ExemploPOO/Models/Aluno.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    //fazendo herança da prática
+    public class Aluno : Pessoa
+    {
+        public Aluno()
+        {
+            
+        }
+        public Aluno(string nome) : base(nome)
+        {
+            
+        }
+        public double Nota { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, e sou um aluno nota {Nota}");
+        }
+    }
+}
---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,147 @@
+﻿using ExemploPOO.Interfaces;
+using ExemploPOO.Models;
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Multiplicar(3, 9)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Computador c = new Computador();
+// Console.WriteLine(c.ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa("Ryan");
+// Aluno a1 = new Aluno("Ryan Carlos");
+// a1.Apresentar();
+// Professor prof = new Professor("Figueiredo");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Corrente c = new Corrente();
+// c.Creditar(500);
+// c.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Aluno a1 = new Aluno();
+// a1.Nome = "Ryan";
+// a1.Idade = 20;
+// a1.Nota = 10;
+// a1.Apresentar();
+
+// Professor p1 = new Professor();
+// p1.Nome = "Ryan";
+// p1.Idade = 20;
+// p1.Salario = 70000;
+// p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Ryan";
+// p1.Idade = 20;
+
+// p1.Apresentar();
